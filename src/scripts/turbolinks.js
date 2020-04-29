ad9394271d5dee51d77fe3bb1be0ef7de5158d13
José Sousa <josesousa9000@gmail.com>
2020-04-29 19:46:14 +0100

    Prefetch on hover

--- src/scripts/turbolinks.js
+++ src/scripts/turbolinks.js
@@ -1,3 +1,9 @@
 const Turbolinks = require("turbolinks");
 
 Turbolinks.start();
+
+// Require this to prefetch links on hover
+window.addEventListener("DOMContentLoaded", () => {
+  // eslint-disable-next-line global-require
+  require("instant.page");
+});
